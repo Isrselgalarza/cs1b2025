86adcbbeee2c5d8728dd88020d0ef819f4a83cfb
Etnan Galarza <egalarza0@saddleback.edu>
2025-02-19 22:23:07 +0000

    new

--- HW04/01.cpp
+++ HW04/01.cpp
@@ -1,22 +1,55 @@
 #include<iostream>
 using namespace std;
 
-enum class dayOfWeek {monday, tuesday, wednesday, thursday, friday, saturday, sunday};
+enum class dayOfWeek { Monday = 1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
 
-vid printNextDay (dayOfWeek day){
-
-    
+dayOfWeek getNextDay(dayOfWeek today) {
+    int todayInt = int(today);
+    if (todayInt == 7){
+        return dayOfWeek::Monday; 
+    }
+    return dayOfWeek(todayInt + 1); 
 }
 
-int main(){
+void printDay(dayOfWeek nextday) {
+    switch (nextDay) {
+        case dayOfWeek::Monday: 
+        cout << "Monday"; 
+        break;
+        case dayOfWeek::Tuesday: 
+        cout << "Tuesday"; 
+        break;
+        case dayOfWeek::Wednesday: 
+        cout << "Wednesday"; 
+        break;
+        case dayOfWeek::Thursday: 
+        cout << "Thursday"; 
+        break;
+        case dayOfWeek::Friday: 
+        cout << "Friday"; 
+        break;
+        case dayOfWeek::Saturday: 
+        cout << "Saturday"; 
+        break;
+        case dayOfWeek::Sunday:
+        cout << "Sunday"; 
+        break;
+    }
+}
 
-    int day;
+int main() {
+    int todayNum;
+    cout << "Enter the current day (1 for Monday, 2 for Tuesday, ..., 7 for Sunday): ";
+    cin >> todayNum;
 
-    cout << "Enter a day using a number" << endl;
+    if (todayNum < 1 || todayNum > 7) {
+        cout << "Invalid input! Please enter a number between 1 and 7." << endl;
+        return 1;
+    }
 
-    cin >> day;
+    dayOfWeek today = dayOfWeek(today);
+    dayOfWeek nextDay = getNextDay(nextDay);
 
-    printNextDay(num);
-
-
+    cout << "The next day is: " << printDay(nextDay) << endl;
+return 0;
 }
--- HW04/04.cpp
+++ HW04/04.cpp
@@ -1,27 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int sortArray(int array[]){
-    int tempNum;
-    if(int i = 0; int j = 1; array[i] < array[j]; j++){
-        for(array[i] > array[j]; i++){
-            tempNum = array[i];
-            array[i] = array[j];
-            array[j] = tempNum;
+void sortArray(int array[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
         }
     }
 }
 
-void printArray(int array[]) {
-    for(int i = 0; i < 5; i++) { 
-        cout << array[i] << ", ";
+int findLongestConsecutive(int array[], int n) {
+    if (n == 0) return 0;
+    sortArray(array, n);
+    int longestStreak = 1, currentStreak = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (array[i] == array[i - 1]) {
+            continue;
+        } else if (array[i] == array[i - 1] + 1) {
+            currentStreak++;
+        } else {
+            if (currentStreak > longestStreak) {
+                longestStreak = currentStreak;
+            }
+            currentStreak = 1;
+        }
     }
+
+    return (currentStreak > longestStreak) ? currentStreak : longestStreak;
 }
 
-int main (){
-    int array[] = {100, 4, 200, 1, 3, 4};
-    sortArray(array);
-    printArray(array);
-
-return 0;
+int main() {
+    int array[] = {100, 4, 200, 1, 3, 2};
+    int n = 6;
+    cout << "Longest consecutive sequence length: " << findLongestConsecutive(array, n) << endl;
+    return 0;
 }
