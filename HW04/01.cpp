71bf99fcc4b43af52891d73c5b173c7b84838d17
Etnan Galarza <egalarza0@saddleback.edu>
2025-02-19 01:41:39 +0000

    new

---
+++ HW04/01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+enum class dayOfWeek {monday, tuesday, wednesday, thursday, friday, saturday, sunday};
+
+void printNextDay (int num){
+
+}
+
+int main(){
+
+    int num;
+
+    cout << "Enter a day using a number" << endl;
+
+
+}
